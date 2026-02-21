b474153eb3d0d33b4e2da0e6539e382bb521d20d
a.salzillo <noctisbels@gmail.com>
2026-02-21 14:54:39 +0100

    chore: update Vite config to explicitly pass empty config to react plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "node:path";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({})],
   root: "src/ui",
   build: {
     outDir: path.resolve(__dirname, "dist/ui"),
